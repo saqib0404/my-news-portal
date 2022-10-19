baae95a2994c7b38a745830391b982e32553bffd
Saqib Ahmad <you@example.com>
2022-10-19 15:09:16 +0600

    right -side navigation and gestures added

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaYoutube, FaTwitch, FaWhatsapp, FaDiscord } from 'react-icons/fa';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right</h2>
+            <ButtonGroup vertical>
+                <Button className='px-5 mb-2 rounded' variant="outline-primary"><FaGoogle></FaGoogle> Login via Google</Button>
+                <Button className='mb-4 rounded' variant="outline-dark"><FaGithub></FaGithub> Login via Github</Button>
+            </ButtonGroup>
+            <h5>Find us on</h5>
+            <ListGroup className='mb-4'>
+                <ListGroup.Item className='mb-2 rounded'><FaFacebook /> Facebook</ListGroup.Item>
+                <ListGroup.Item className='mb-2 rounded'><FaYoutube /> Youtube</ListGroup.Item>
+                <ListGroup.Item className='mb-2 rounded'><FaTwitch /> Twitter</ListGroup.Item>
+                <ListGroup.Item className='mb-2 rounded'><FaWhatsapp /> Whatsapp</ListGroup.Item>
+                <ListGroup.Item className='mb-2 rounded'><FaDiscord /> Discord</ListGroup.Item>
+            </ListGroup>
+            <h5>Brand Partners</h5>
+            <BrandCarousel></BrandCarousel>
         </div>
     );
 };
